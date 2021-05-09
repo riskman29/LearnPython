7a32b077def06ff66cfdb63e33910d880a42afcf
riskman29 <82551719+riskman29@users.noreply.github.com>
2021-05-09 13:31:22 -0400

    helloWorld file created

---
+++ helloWorld.py
@@ -0,0 +1,17 @@
+#This program says hello, asks for my name and password.
+
+print ("Hello!")
+print ("What is you name?")
+myName = input()
+
+if myName == 'Dale':
+    print ("Welcome back, " + myName + ".")
+    print ("What is your password? ")
+    myPassword = input()
+    if myPassword == "risky":
+        print("Access granted.")
+    else:
+        print("Wrong password. Bye stranger!")
+else:
+    print("Hello, stranger")
+
